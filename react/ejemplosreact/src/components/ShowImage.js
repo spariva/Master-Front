89945577f6031bace26940740fd8d6b4c4a49e31
Maki Spariva <makspariva@gmail.com>
2024-10-10 12:53:49 +0200

    eliminar proyecto

--- react/ejemplosreact/src/components/ShowImage.js
+++ react/ejemplosreact/src/components/ShowImage.js
@@ -1,8 +1,13 @@
+import luna from '../assets/images/luna.jpg';
+
 export function ShowImage() {
     return (
         <>
             <img
-                src="" width={400} alt="texto"
+                src="../assets/images/luna.jpg" width={400} alt="Este no funciona"
+            />
+            <img
+                src={luna} width={400} alt="Así sí funciona"
             />
             <img
                 src="https://i.imgur.com/7vQD0fPs.jpg"
