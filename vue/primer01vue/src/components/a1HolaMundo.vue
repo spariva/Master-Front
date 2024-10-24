e9db7a3c8a8e56a89d076c05a233f2a35a042bb0
Maki Spariva <makspariva@gmail.com>
2024-10-24 14:16:06 +0200

    Primer Vue

---
+++ vue/primer01vue/src/components/a1HolaMundo.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>Mensajito</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "a1HolaMundo",
+};
+</script>
+
+<style>
+h1 {
+  color: rgb(67, 67, 185);
+}
+</style>
