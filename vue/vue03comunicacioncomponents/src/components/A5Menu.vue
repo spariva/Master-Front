e4225ef42b14db6e4719fa4666e570760e662ca0
Maki Spariva <makspariva@gmail.com>
2024-10-30 14:23:44 +0100

    routes

--- vue/vue03comunicacioncomponents/src/components/A5Menu.vue
+++ vue/vue03comunicacioncomponents/src/components/A5Menu.vue
@@ -6,7 +6,9 @@
             <li><router-link to="/checkboxes">Checkboxes</router-link></li>
             <li><router-link to="/numerodoble">No param</router-link></li>
             <li><router-link to="/numerodoble/13">Param 13</router-link></li>
-            <li><router-link to="/numerodoble/13">Param 13</router-link></li>
+            <li v-for="num in numeros" :key="num">
+                <router-link :to="'/numerodoble/' + num">{{ num }}</router-link>
+            </li>
         </ul>
     </div>
 </template>
@@ -14,6 +16,11 @@
 <script>
 export default {
     name: "A5Menu",
+    data(){
+        return{
+            numeros: [1,2,3,4]
+        }
+    }
 }
 </script>
 
